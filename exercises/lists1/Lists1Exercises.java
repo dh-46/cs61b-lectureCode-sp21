889866c83396b644c7656187d247ba737770eac9
Daniel Huang <danielhuang.work@gmail.com>
2024-12-29 11:39:09 +0800

    Lectore4 exercise

--- exercises/lists1/Lists1Exercises.java
+++ exercises/lists1/Lists1Exercises.java
@@ -4,7 +4,15 @@ public class Lists1Exercises {
       * to change. */
     public static IntList incrList(IntList L, int x) {
         /* Your code here. */
-        return L;        
+        IntList head = null;
+
+        // 倒過來找，最後留下來的才會是 List 的頭
+        for (int i = L.size() - 1; i >= 0; i--) {
+          int value = L.get(i);
+          int newValue = value + x;
+          head = new IntList(newValue, head);
+        }
+        return head;
     }
 
     /** Returns an IntList identical to L, but with
@@ -12,6 +20,11 @@ public class Lists1Exercises {
       * the 'new' keyword. */
     public static IntList dincrList(IntList L, int x) {
         /* Your code here. */
+        IntList pointer = L;
+        while (pointer != null) {
+          pointer.first = pointer.first + x;
+          pointer = pointer.rest;
+        }
         return L;
     }
 
@@ -26,8 +39,8 @@ public class Lists1Exercises {
         // Test your answers by uncommenting. Or copy and paste the
         // code for incrList and dincrList into IntList.java and
         // run it in the visualizer.
-        // System.out.println(L.get(1));
-        // System.out.println(incrList(L, 3));
-        // System.out.println(dincrList(L, 3));        
+        System.out.println(L.get(1));
+        System.out.println(incrList(L, 3));
+        System.out.println(dincrList(L, 3));        
     }
 }
