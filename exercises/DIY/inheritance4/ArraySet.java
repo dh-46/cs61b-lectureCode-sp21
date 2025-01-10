13287cb1f64954439d07a65fbd4cd16cc4b93918
Daniel Huang <daniel.h@emct.tw>
2025-01-10 14:37:35 +0800

    Lecture 11-ArraySets-Basic ArraySet

--- exercises/DIY/inheritance4/ArraySet.java
+++ exercises/DIY/inheritance4/ArraySet.java
@@ -1,28 +1,43 @@
 public class ArraySet<T> {
 
+    private T[] items;
+    private int size; // the next item to be added will be position "size"
+
     public ArraySet() {
+        items = (T[]) new Object[100];
+        size = 0;
     }
 
     /* Returns true if this map contains a mapping for the specified key.
      */
     public boolean contains(T x) {
+        for (int i = 0; i < size; i++) {
+            if (items[i].equals(x)) {
+                return true;
+            }
+        }
+
         return false;
     }
 
     /* Associates the specified value with the specified key in this map. 
        Throws an IllegalArgumentException if the key is null. */
     public void add(T x) {
-        return;
+        if (contains(x)) {
+            return;
+        }
+        items[size] = x;
+        size += 1;
     }
 
     /* Returns the number of key-value mappings in this map. */
     public int size() {
-        return 0;
+        return size;
     }
 
     public static void main(String[] args) {
         ArraySet<String> s = new ArraySet<>();
-        s.add(null);
+//        s.add(null);
         s.add("horse");
         s.add("fish");
         s.add("house");
